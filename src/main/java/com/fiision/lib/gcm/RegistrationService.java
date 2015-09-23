ef9505a0a12b31697f9fe490acecc8db2f1783d5
Phuc, Tran Huu <phuctran0302@gmail.com>
2015-09-23 09:38:06 +0700

    Initialzie code.

---
+++ src/main/java/com/fiision/lib/gcm/RegistrationService.java
@@ -0,0 +1,95 @@
+package com.fiision.lib.gcm;
+
+
+import android.app.*;
+import android.content.*;
+import android.preference.*;
+import android.support.v4.content.*;
+import android.util.*;
+
+import com.google.android.gms.gcm.*;
+import com.google.android.gms.iid.*;
+
+import java.io.*;
+
+
+public final class RegistrationService extends IntentService {
+
+
+    private static final String TAG = "RegistrationService";
+    private static final String[] TOPICS = {"global"};
+
+    public RegistrationService() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        try {
+            String authorizedEntity = "560694976723"; // Project id from Google Developers Console
+            String scope = "GCM"; // e.g. communicating using GCM, but you can use any
+            // URL-safe characters up to a maximum of 1000, or
+            // you can also leave it blank.
+
+
+            // [START register_for_gcm]
+            // Initially this call goes out to the network to retrieve the token, subsequent calls
+            // are local.
+            // [START get_token]
+            InstanceID instanceID = InstanceID.getInstance(this);
+            String token = instanceID.getToken(authorizedEntity, GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
+            // [END get_token]
+            Log.i(TAG, "GCM Registration Token: " + token);
+
+            // TODO: Implement this method to send any registration to your app's servers.
+            sendRegistrationToServer(token);
+
+            // Subscribe to topic channels
+            subscribeTopics(token);
+
+            // You should store a boolean that indicates whether the generated token has been
+            // sent to your server. If the boolean is false, send the token to your server,
+            // otherwise your server should have already received the token.
+            sharedPreferences.edit().putBoolean(QuickstartPreferences.SENT_TOKEN_TO_SERVER, true).apply();
+            // [END register_for_gcm]
+        } catch (Exception e) {
+            Log.d(TAG, "Failed to complete token refresh", e);
+            // If an exception happens while fetching the new token or updating our registration data
+            // on a third-party server, this ensures that we'll attempt the update at a later time.
+            sharedPreferences.edit().putBoolean(QuickstartPreferences.SENT_TOKEN_TO_SERVER, false).apply();
+        }
+        // Notify UI that registration has completed, so the progress indicator can be hidden.
+        Intent registrationComplete = new Intent(QuickstartPreferences.REGISTRATION_COMPLETE);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(registrationComplete);
+    }
+
+    /**
+     * Persist registration to third-party servers.
+     *
+     * Modify this method to associate the user's GCM registration token with any server-side account
+     * maintained by your application.
+     *
+     * @param token The new token.
+     */
+    private void sendRegistrationToServer(String token) {
+        // Add custom implementation, as needed.
+    }
+
+    /**
+     * Subscribe to any GCM topics of interest, as defined by the TOPICS constant.
+     *
+     * @param token GCM token
+     * @throws java.io.IOException if unable to reach the GCM PubSub service
+     */
+    // [START subscribe_topics]
+    private void subscribeTopics(String token) throws IOException {
+        for (String topic : TOPICS) {
+            GcmPubSub pubSub = GcmPubSub.getInstance(this);
+            pubSub.subscribe(token, "/topics/" + topic, null);
+        }
+    }
+    // [END subscribe_topics]
+
+}
