4abe9af9be88788ddeb476e9af2105e872ac50e0
FiisionStudio <phuc@phuc-macbook13.local>
2015-10-05 16:46:59 +0700

    Implemented.

---
+++ src/main/java/com/fiision/lib/gcm/FwiGcmListenerService.java
@@ -0,0 +1,98 @@
+//  Project name: FwiGcm
+//  File name   : FwiGcmListenerService.java
+//
+//  Author      : Phuc, Tran Huu
+//  Created date: 8/17/15
+//  Version     : 1.00
+//  --------------------------------------------------------------
+//  Copyright (C) 2012, 2015 Fiision Studio.
+//  All Rights Reserved.
+//  --------------------------------------------------------------
+//
+//  Permission is hereby granted, free of charge, to any person obtaining  a  copy
+//  of this software and associated documentation files (the "Software"), to  deal
+//  in the Software without restriction, including without limitation  the  rights
+//  to use, copy, modify, merge,  publish,  distribute,  sublicense,  and/or  sell
+//  copies of the Software,  and  to  permit  persons  to  whom  the  Software  is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF  ANY  KIND,  EXPRESS  OR
+//  IMPLIED, INCLUDING BUT NOT  LIMITED  TO  THE  WARRANTIES  OF  MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO  EVENT  SHALL  THE
+//  AUTHORS OR COPYRIGHT HOLDERS  BE  LIABLE  FOR  ANY  CLAIM,  DAMAGES  OR  OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING  FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN  THE
+//  SOFTWARE.
+//
+//
+//  Disclaimer
+//  __________
+//  Although reasonable care has been taken to  ensure  the  correctness  of  this
+//  software, this software should never be used in any application without proper
+//  testing. Fiision Studio disclaim  all  liability  and  responsibility  to  any
+//  person or entity with respect to any loss or damage caused, or alleged  to  be
+//  caused, directly or indirectly, by the use of this software.
+
+package com.fiision.lib.gcm;
+
+
+import android.app.*;
+import android.content.*;
+import android.media.*;
+import android.net.*;
+import android.os.*;
+import android.support.v4.app.*;
+
+import com.google.android.gms.gcm.*;
+
+import java.io.*;
+import java.util.*;
+
+
+public class FwiGcmListenerService extends GcmListenerService {
+
+
+    @Override
+    public void onMessageReceived(String from, Bundle data) {
+        Intent broadcastIntent = new Intent(FwiGcmListenerService.class.getName());
+        Set<String> keys = data.keySet();
+
+        for (String key : keys) {
+            /* Condition validation: Skip collapse key */
+            if (key.compareTo("collapse_key") == 0) continue;
+
+            Serializable value = data.getSerializable(key);
+            broadcastIntent.putExtra(key, value);
+        }
+        sendBroadcast(broadcastIntent);
+    }
+
+    /**
+     * Create and show a simple notification containing the received GCM message.
+     *
+     * @param message GCM message received.
+     */
+    private void sendNotification(String message) {
+//        Intent intent = new Intent(this, MapActivity.class);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+//                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+//                .setSmallIcon(R.drawable.ic_stat_ic_notification)
+                .setContentTitle("GCM Message")
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri);
+//                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+}
